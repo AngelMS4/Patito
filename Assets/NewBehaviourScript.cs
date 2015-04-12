b713c5f5825d16ddbc0e7b355ab177790ca8f0e5
Ángel Santana <martinezsantanaangel@gmail.com>
2015-04-12 12:20:44 -0500

    Recommiting

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+  Debug.Log("Hola");
+}
+
+function Update () {
+	var vertical = Input.GetAxis("Vertical");
+	var horizontal = Input.GetAxis("Horizontal");
+	var movement = new Vector3(horizontal, 0, vertical);
+	GetComponent.<Rigidbody>().AddForce(movement);
+}
